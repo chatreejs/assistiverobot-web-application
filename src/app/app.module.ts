879acf7d88de61bed58612d5cc8a7a64d6e4da3b
Chanon Treemeth <v_chanont@ctlsd.set>
2020-04-03 19:52:38 +0700

    Add theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgZorroAntdModule, NZ_I18N, th_TH } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import th from '@angular/common/locales/th';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
+
+registerLocaleData(th);
 
 @NgModule({
   declarations: [
@@ -10,9 +22,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzGridModule,
+    NzSelectModule,
+    NzButtonModule,
+    NgZorroAntdMobileModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: th_TH }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
