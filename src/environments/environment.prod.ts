eefdf44897fffe091099a4175956745131940444
CHANON TREEMETH <chanon.t@cdg.co.th>
2020-07-25 17:45:36 +0700

    Edit Production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,41 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://192.168.1.106/service',
+  mock_data: [{
+    point: '1801',
+    position: {
+      x: -0.70454,
+      y: -2.72520,
+      z: 0
+    }, orientation: {
+      x: 0,
+      y: 0,
+      z: -0.70232,
+      w: 0.708880
+    }
+  }, {
+    point: '1802',
+    position: {
+      x: 0.51862,
+      y: 1.48194,
+      z: 0
+    }, orientation: {
+      x: 0,
+      y: 0,
+      z: 0.09697,
+      w: 0.99528
+    }
+  }, {
+    point: '1815',
+    position: {
+      x: 0,
+      y: -2,
+      z: 0
+    }, orientation: {
+      x: 0,
+      y: 0,
+      z: 0,
+      w: 1
+    }
+  }]
 };
