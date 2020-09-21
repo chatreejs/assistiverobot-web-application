4cab34b0e824a23b8c475c3d129692baa56eb19e
CHANON TREEMETH <chanon.t@cdg.co.th>
2020-09-21 09:47:39 +0700

    Add admin component. and routing

--- projects/website/src/app/admin/admin-routing.module.ts
+++ projects/website/src/app/admin/admin-routing.module.ts
@@ -1,12 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { JobComponent } from './job/job.component';
+import { LocationComponent } from './location/location.component';
+import { UmComponent } from './um/um.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    component: AdminComponent
+    component: AdminComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      {
+        path: 'location',
+        component: LocationComponent
+      },
+      {
+        path: 'job',
+        component: JobComponent
+      },
+      {
+        path: 'um',
+        component: UmComponent
+      }
+    ]
   }
 ];
 
--- projects/website/src/app/admin/admin.module.ts
+++ projects/website/src/app/admin/admin.module.ts
@@ -3,13 +3,30 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { LocationComponent } from './location/location.component';
+import { JobComponent } from './job/job.component';
+import { UmComponent } from './um/um.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [
+    AdminComponent,
+    LocationComponent,
+    JobComponent,
+    UmComponent,
+    DashboardComponent
+  ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+
+    NzLayoutModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
   ]
 })
 export class AdminModule { }
