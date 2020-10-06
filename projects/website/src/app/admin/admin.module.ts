39d13cef15735f1d0240069d8c82aea5dbd4eca9
Aukrit Kerdsiri <aukrit.java@g.swu.ac.th>
2020-10-06 22:38:39 +0700

    fornt

--- projects/website/src/app/admin/admin.module.ts
+++ projects/website/src/app/admin/admin.module.ts
@@ -10,8 +10,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-
-
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzFormModule } from 'ng-zorro-antd/form';
 @NgModule({
   declarations: [
     AdminComponent,
@@ -27,6 +34,14 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
     NzLayoutModule,
     NzMenuModule,
     NzBreadCrumbModule,
+    NzButtonModule,
+    NzCheckboxModule,
+    NzRadioModule,
+    NzTableModule,
+    NzIconModule,
+    NzToolTipModule,
+    NzInputModule,
+    NzFormModule
   ]
 })
 export class AdminModule { }
