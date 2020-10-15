1b8dab01f1c3049149cd775d32fae58f3ab972f7
thanathipch9@gmail.com <Oat141143>
2020-10-15 21:39:04 +0700

    add Form to login component and set responsive

--- projects/website/src/app/authentication/authentication.module.ts
+++ projects/website/src/app/authentication/authentication.module.ts
@@ -3,6 +3,16 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
 
 
 @NgModule({
@@ -11,7 +21,18 @@ import { AuthenticationRoutingModule } from './authentication-routing.module';
   ],
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+
+    NzLayoutModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
+    NzGridModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzIconModule,
+    NzInputModule,
+    NzButtonModule,
+    NzCardModule
   ],
   providers: []
 })
